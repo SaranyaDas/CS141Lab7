9785c902ff65363bdf3a8bd8db65f43e1df0b507
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 02:14:55 +0530

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
- 
-void factorial(int n,int fact){
-	if(n>0){
-		fact*=n;
+void factorial(int n, int fact) {
+	if(n > 0) {
+		fact *= n;
 		--n;
-		factorial(n,fact);
+		factorial(n, fact);
 	}
-	else
-		cout<<"Factorial equals:"<<fact;
+	else {
+		cout << "Factorial =" << fact;
+	}
 }
- 
 int main() {
-	int n,fact=1;
-	cout<<"Enter your number:";
-	cin>>n;
-	factorial(n,fact);
+	int n, fact = 1;
+	cout << "Enter a number: ";
+	cin >> n;
+	factorial(n, fact);
 	return 0;
 }
