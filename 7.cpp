6cd5406c145b2d4d3d7aa0a72346115cd0bbb5e9
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 02:04:24 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
- 
-void reverse(int n,int dummy,int rev){
-	if(n>0){
-		rev=rev*10+(n%10);
-		n/=10;
-		reverse(n,dummy,rev);
+void reverse(int n, int dummy, int rev) {
+	if (n > 0) {
+		rev = rev*10 + (n%10);
+		n /= 10;
+		reverse(n, dummy, rev);
 	}
-	else
-		if(dummy==rev)
-			cout<<"Input is palindrome";
-		else	
-			cout<<"Input is not palindrome";
+	else {
+		if(dummy == rev)
+			cout << "Input is a palindrome." << endl;
+		else {
+			cout << "Input is not a palindrome." << endl;
+		}
 }
- 
 int main() {
-	int n,dummy,rev=0;
-	cout<<"Enter your number:";
-	cin>>n;
-	dummy=n;
-	reverse(n,dummy,rev);
+	int n, dummy, rev = 0;
+	cout << "Enter a number: ";
+	cin >> n;
+	dummy = n;
+	reverse(n, dummy, rev);
 	return 0;
 }
