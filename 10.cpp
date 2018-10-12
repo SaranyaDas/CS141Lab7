49f22199e820e18b03f1faa64d579988e94fda28
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 02:13:17 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-
-void fibonacci(int n,int num1,int num2){
-	if(n==1)
-		cout<<"Desired Sequence Term:"<<num1;
-	else
-		fibonacci(--n,num2,num1+num2);
+void fibonacci(int n, int n1, int n2) {
+	if (n == 1) {
+		cout << "Desired Sequence Term: " << n1;
+	}
+	else {
+		fibonacci(--n, n2, n1 + n2);
+	}
 }
-
 int main() {
-	int num1=1,num2=1,n;
-	cout<<"Enter term position";
-	cin>>n;
-	fibonacci(n,num1,num2);
+	int n1 = 1, n2= 1, n;
+	cout << "Enter term position: ";
+	cin >> n;
+	fibonacci(n, n1, n2);
 	return 0;
 }
