effdb11df3f51dad3cbe656e41d4f399e3ec8c02
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 00:45:45 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
-
-void print(int i,int n){
-	if(i>=0){
-	cout<<(n-i)<<endl;
-	print(--i,n);
+void print(int i, int n) {
+	if (i>=0) {
+		cout << (n-i) << endl;
+		print(--i, n);
 	}
 }
-
 int main() {
 	int n;
-	cout<<"Enter your Number:";
-	cin>>n;
-	print(n-1,n);
+	cout << "Enter a number: ";
+	cin >> n << endl;
+	print(n-1, n);
 	return 0;
 }
