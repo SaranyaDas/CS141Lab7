41a04b4003dabbe556d7de622a04fda4b887267b
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 02:00:24 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
-
 void reverse(int n, int rev){
-	if(n>0){
-		rev=rev*10+(n%10);
-		n/=10;
-		reverse(n,rev);
+	if (n > 0) {
+		rev = rev*10 + (n%10);
+		n /= 10;
+		reverse (n, rev);
 	}
-	else
-		cout<<"Reversed Number:"<<rev;
+	else {
+		cout << "Reversed number: " << rev << endl;
+	}
 }
-
 int main() {
-	int n,rev=0;
-	cout<<"Enter your number:";
-	cin>>n;
-	reverse(n,rev);
+	int n, rev = 0;
+	cout << "Enter a number: ";
+	cin >> n;
+	reverse (n, rev);
 	return 0;
 }
