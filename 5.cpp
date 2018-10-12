42c145443e1ac89c9f5bff089b046b5472451e5f
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 01:48:34 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
- 
-void odd_even (int ch, int n, int sum){
-	if(ch<=n){
-		sum+=ch;
-		odd_even((++(++ch)),n,sum);
+void oddeven(int type, int n, int sum) {
+	if (type <= n) {
+		sum += type;
+		oddeven((++(++type)), n, sum);
 	}
-	else
-		cout<<"Sum equals:"<<sum;
+	else {
+		cout << "Sum = "<< sum << endl;
+	}
 }
-
 int main() {
-	int ch,n,sum;
-	cout<<"Enter 0 for EVEN or 1 for ODD:";
-	cin>>ch;
-	cout<<"Enter terminating number:";
-	cin>>n;
-	odd_even(ch,n,sum);
+	int type, n, sum;
+	cout << "Enter 0 for EVEN or 1 for ODD: ";
+	cin >> type;
+	cout << "Enter terminating number: ";
+	cin >> n;
+	oddeven(type, n, sum);
 	return 0;
 }
