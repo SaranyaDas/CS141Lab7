a14387d37032ff2baaa33a49d9ef985ec1fd421e
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 00:50:13 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-
-void odd_even(int ch,int n){
-	if(ch<=n){
-		cout<<ch<<endl;
-		odd_even(++(++ch),n);
+void oddeven(int type, int n){
+	if (type <= n) {
+		cout << type << endl;
+		oddeven(++(++type), n);
 	}
-	
 }
-
 int main() {
-	int ch,n;
-	cout<<"Enter 0 for EVEN or 1 for ODD:";
-	cin>>ch;
-	cout<<"Enter terminating number:";
-	cin>>n;
-	odd_even(ch,n);
-	return 0;
+	int type, n;
+	cout << "Enter 0 for EVEN or 1 for ODD: ";
+	cin >> type;
+	cout << "Enter terminating number: ";
+	cin >> n;
+	oddeven(type, n);
+	return 0; 
 }
