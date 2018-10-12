7ed44e34b10d69c9be9482f792130990f29b209a
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-10-13 02:07:33 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
- 
-void sum_digits(int n,int sum){
-	if(n>0){
-		sum+=(n%10);
-		n/=10;
-		sum_digits(n,sum);
+void sumdigits(int n, int sum) {
+	if (n > 0) {
+		sum += (n%10);
+		n /= 10;
+		sumdigits(n, sum);
 	}
-	else
-		cout<<"Sum of digits equals:"<<sum;
+	else {
+		cout << "Sum of digits= " << sum << endl;
+	}
 }
- 
 int main() {
-	int n,sum=0;
-	cout<<"Enter your number:";
-	cin>>n;
-	sum_digits(n,sum);
+	int n, sum = 0;
+	cout << "Enter a number: ";
+	cin >> n;
+	sumdigits(n, sum);
 	return 0;
 }
